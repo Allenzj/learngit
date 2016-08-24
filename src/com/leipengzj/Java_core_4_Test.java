0368d61b254fe251dca165eafc75a23bcebe2f1d
penglei <penglei@asiainfo.com>
2016-08-24 15:50:40 +0800

    初次提交，无新增内容

---
+++ src/com/leipengzj/Java_core_4_Test.java
@@ -0,0 +1,11 @@
+package com.leipengzj;
+
+public class Java_core_4_Test {
+
+	 static{
+		 System.out.println("new ...");
+	 }
+	 public static void main(String[] args) {
+		System.out.println();
+	}
+}
